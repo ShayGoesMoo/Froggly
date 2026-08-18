64fe0199960b997c49572e0d81113b1bcff7f881
ShayGoesMoo <shakirforrest19@gmail.com>
2026-08-18 14:10:01 -0500

    started /new/

---
+++ new/new.js
@@ -0,0 +1,7 @@
+const filePanel = document.getElementById("file-panel");
+const fileInput = document.getElementById("file-input");
+const filePlaceholder = document.getElementById("file-placeholder");
+const previewWrapper = document.getElementById("preview-wrapper");
+const previewFile = document.getElementById("preview-file");
+const changeFileBtn = document.getElementById("change-file-btn");
+const removeFileBtn = document.getElementById("remove-file-btn");
